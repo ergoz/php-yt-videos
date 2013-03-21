ee647ce444c1496a14f91f4b3deca98157836369
Benjamin David <ben@dukt.net>
2013-03-21 03:41:54 +0100

    Saving service parameters in session

--- example/index.php
+++ example/index.php
@@ -24,7 +24,34 @@ $app->get('/', function() use ($app) {
     return $app['twig']->render('index.twig', array(
         'services' => $services,
     ));
+});
 
+// service settings
+$app->get('/services/{name}', function($name) use ($app) {
+    $service = Dukt\Videos\Common\ServiceFactory::create($name);
+    $sessionVar = 'dukt.videos.'.$service->getShortName();
+
+    $service->initialize((array) $app['session']->get($sessionVar));
+
+    return $app['twig']->render('service.twig', array(
+        'service' => $service,
+        'settings' => $service->getParameters(),
+    ));
+});
+
+// post service settings
+$app->post('/services/{name}', function($name) use ($app) {
+    $service = Dukt\Videos\Common\serviceFactory::create($name);
+    $sessionVar = 'dukt.videos.'.$service->getShortName();
+    $service->initialize((array) $app['request']->get('service'));
+
+    // save service settings in session
+    $app['session']->set($sessionVar, $service->getParameters());
+
+    // redirect back to service settings page
+    $app['session']->getFlashBag()->add('success', 'Service settings updated!');
+
+    return $app->redirect($app['request']->getPathInfo());
 });
 
 $app->run();
